70a61ab5292ff8ca61c595d6c4f5a2a983bb1694
aquova <austinbricker@protonmail.com>
2022-12-14 15:03:02 -0600

    Fix main AOC function table

--- aoc.nim
+++ aoc.nim
@@ -1,5 +1,7 @@
 import tables
 
+type DayProc = proc(input: string): string
+
 import day1/day1
 import day2/day2
 import day3/day3
@@ -16,20 +18,20 @@ import day13/day13
 import day14/day14
 
 const FUNCTION_TABLE = {
-  "day1p1": day1p1, "day1p2": day1p2,
-  "day2p1": day2p1, "day2p2": day2p2,
-  "day3p1": day3p1, "day3p2": day3p2,
-  "day4p1": day4p1, "day4p2": day4p2,
-  "day5p1": day5p1, "day5p2": day5p2,
-  "day6p1": day6p1, "day6p2": day6p2,
-  "day7p1": day7p1, "day7p2": day7p2,
-  "day8p1": day8p1, "day8p2": day8p2,
-  "day9p1": day9p1, "day9p2": day9p2,
-  "day10p1": day10p1, "day10p2": day10p2,
-  "day11p1": day11p1, "day11p2": day11p2,
-  "day12p1": day12p1, "day12p2": day12p2,
-  # "day13p1": day13p1, "day13p2": day13p2,
-  "day14p1": day14p1, "day14p2": day14p2,
+  "day1p1": DayProc(day1p1), "day1p2": DayProc(day1p2),
+  "day2p1": DayProc(day2p1), "day2p2": DayProc(day2p2),
+  "day3p1": DayProc(day3p1), "day3p2": DayProc(day3p2),
+  "day4p1": DayProc(day4p1), "day4p2": DayProc(day4p2),
+  "day5p1": DayProc(day5p1), "day5p2": DayProc(day5p2),
+  "day6p1": DayProc(day6p1), "day6p2": DayProc(day6p2),
+  "day7p1": DayProc(day7p1), "day7p2": DayProc(day7p2),
+  "day8p1": DayProc(day8p1), "day8p2": DayProc(day8p2),
+  "day9p1": DayProc(day9p1), "day9p2": DayProc(day9p2),
+  "day10p1": DayProc(day10p1), "day10p2": DayProc(day10p2),
+  "day11p1": DayProc(day11p1), "day11p2": DayProc(day11p2),
+  "day12p1": DayProc(day12p1), "day12p2": DayProc(day12p2),
+  "day13p1": DayProc(day13p1), "day13p2": DayProc(day13p2),
+  "day14p1": DayProc(day14p1), "day14p2": DayProc(day14p2),
 }.toTable()
 
 when defined(js):
