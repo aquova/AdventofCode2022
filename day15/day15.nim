e5a441c48645bfe57062b09082ad78a0eabc36cd
aquova <austinbricker@protonmail.com>
2022-12-15 11:12:21 -0600

    Improve runtime of day 15 part 1

--- day15/day15.nim
+++ day15/day15.nim
@@ -1,4 +1,4 @@
-import sets
+from algorithm import sorted
 import strscans
 import strutils
 
@@ -11,6 +11,42 @@ type Point = tuple
 type Pair = object
     sensor: Point
     beacon: Point
+
+type Range = object
+    left, right: int
+
+proc newRange(left: int, right: int): Range =
+    result.left = left
+    result.right = right
+
+proc contains(r:Range, v: int): bool =
+    return r.left <= v and v <= r.right
+
+proc overlaps(a: Range, b: Range): bool =
+    return a.contains(b.left) or a.contains(b.right)
+
+proc merge(a: Range, b: Range): Range =
+    return newRange(min(a.left, b.left), max(a.right, b.right))
+
+proc cmp(a: Range, b: Range): int =
+    return system.cmp(a.left, b.left)
+
+proc merge(ranges: seq[Range]): seq[Range] =
+    let sorted = sorted(ranges, cmp)
+    var current = sorted[0]
+    var idx = 1
+    while idx < sorted.len():
+        let other = sorted[idx]
+        if current.overlaps(other):
+            current = current.merge(other)
+        else:
+            result.add(current)
+            current = other
+        inc(idx)
+    result.add(current)
+
+proc size(r: Range): int =
+    return r.right - r.left + 1
 
 proc newPair(sensor: Point, beacon: Point): Pair =
     result.sensor = sensor
@@ -47,8 +83,9 @@ proc getEdges(pair: Pair): seq[Point] =
 
 proc day15p1*(input: string): string =
     var
-        empty: HashSet[Point]
         pairs: seq[Pair]
+        ranges: seq[Range]
+        row_beacons: seq[Point]
     for line in input.splitLines():
         let (success, sx, sy, bx, by) = line.scanTuple("Sensor at x=$i, y=$i: closest beacon is at x=$i, y=$i")
         if success:
@@ -60,13 +97,19 @@ proc day15p1*(input: string): string =
         let height = pair.sensor.dist((center_x, TARGET_Y))
         let dx = dst - height
         if dx > 0:
-            for x in countup(center_x - dx, center_x + dx):
-                empty.incl((x, TARGET_Y))
+            let new_range = newRange(center_x - dx, center_x + dx)
+            ranges.add(new_range)
+        if pair.beacon.y == TARGET_Y and pair.beacon notin row_beacons:
+            row_beacons.add(pair.beacon)
 
-    for pair in pairs:
-        empty.excl(pair.beacon)
-
-    return $empty.len()
+    let merged = ranges.merge()
+    var cnt = 0
+    for r in merged:
+        cnt += r.size()
+        for b in row_beacons:
+            if r.contains(b.y):
+                dec(cnt)
+    return $cnt
 
 proc day15p2*(input: string): string =
     var pairs: seq[Pair]
