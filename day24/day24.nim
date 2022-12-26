86c0e3c04dae26a299546bf563af4d135963a352
aquova <austinbricker@protonmail.com>
2022-12-25 21:07:05 -0600

    Finished day 24

--- day24/day24.nim
+++ day24/day24.nim
@@ -21,11 +21,11 @@ type Position = object
     pos: Point[int]
     time: int
 
-iterator neighbors(p: Point[int], walls: Walls): Point[int] =
+iterator neighbors(p: Point[int], walls: Walls, width, height: int): Point[int] =
     for dir in Direction:
         let next = p + DP[dir]
-        if next notin walls and next.x > 0 and next.y > 0:
-            yield p + DP[dir]
+        if next notin walls and next.y >= 0 and next.y < height:
+            yield next
     yield p
 
 proc newPosition(p: Point[int], time: int): Position =
@@ -52,7 +52,6 @@ proc move(b: Blizzard, width, height: int): Blizzard =
         next.x = width - 2
     elif next.x == width - 1:
         next.x = 1
-
     if next.y == 0:
         next.y = height - 2
     elif next.y == height - 1:
@@ -69,52 +68,30 @@ proc contains(blizzards: seq[Blizzard], p: Point[int]): bool =
             return true
     return false
 
-proc `$`(blizzards: seq[Blizzard]): string =
-    for y in countup(0, 36):
-        for x in countup(0, 101):
-            let p = (x, y)
-            var found = false
-            for b in blizzards:
-                if p == b.pos:
-                    found = true
-                    let sym = case b.dir:
-                        of North: '^'
-                        of South: 'v'
-                        of West: '<'
-                        of East: '>'
-                    result.add(sym)
-                    break
-            if not found:
-                result.add('.')
-        result.add("\n")
-
-proc bfs(start: Point[int], target: Point[int], blizzards: seq[Blizzard], walls: Walls, width, height: int): seq[Point[int]] =
+proc bfs(start: Position, target: Point[int], blizzard_timeline: var seq[seq[Blizzard]], walls: Walls, width, height: int): seq[Point[int]] =
     var
         queue: Deque[Position]
         parents: Table[Position, Position]
-        blizzard_timeline: seq[seq[Blizzard]]
         backtrack: Position
 
-    blizzard_timeline.add(blizzards)
-    # echo($blizzards)
-    queue.addLast(newPosition(start, 0))
+    queue.addLast(start)
     while queue.len() > 0:
         let pos = queue.popFirst()
         if pos.pos == target:
             backtrack = pos
             break
 
-        for n in pos.pos.neighbors(walls):
+        for n in pos.pos.neighbors(walls, width, height):
             let new_pos = newPosition(n, pos.time + 1)
             if new_pos.time == blizzard_timeline.len():
                 let next_blizzard = blizzard_timeline[blizzard_timeline.len() - 1].iterateBlizzards(width, height)
                 blizzard_timeline.add(next_blizzard)
-                # echo($next_blizzard)
+
             if not parents.contains(new_pos) and not blizzard_timeline[new_pos.time].contains(new_pos.pos):
                 queue.addLast(new_pos)
                 parents[new_pos] = pos
 
-    while backtrack.pos != start:
+    while backtrack != start:
         result.add(backtrack.pos)
         backtrack = parents[backtrack]
     result = result.reversed()
@@ -124,9 +101,8 @@ proc day24p1*(input: string): string =
     let height = lines.len()
     let width = lines[0].len()
 
-    let start: Point[int] = (1, 0)
+    let start = newPosition((1, 0), 0)
     let target: Point[int] = (width - 2, height - 1)
-    # echo(target)
     let arrows = ['^', '<', '>', 'v']
 
     var
@@ -138,9 +114,37 @@ proc day24p1*(input: string): string =
                 blizzards.add(newBlizzard(x, y, c))
             elif c == '#':
                 walls.add((x, y))
-    let path = bfs(start, target, blizzards, walls, width, height)
-    # echo(path)
+
+    var blizzard_cache: seq[seq[Blizzard]]
+    blizzard_cache.add(blizzards)
+    let path = bfs(start, target, blizzard_cache, walls, width, height)
     return $path.len()
 
 proc day24p2*(input: string): string =
-    discard
+    let lines = input.splitLines()
+    let height = lines.len()
+    let width = lines[0].len()
+
+    let start = newPosition((1, 0), 0)
+    let target: Point[int] = (width - 2, height - 1)
+    let arrows = ['^', '<', '>', 'v']
+
+    var
+        walls: Walls
+        blizzards: seq[Blizzard]
+    for y, line in lines.pairs():
+        for x, c in line:
+            if c in arrows:
+                blizzards.add(newBlizzard(x, y, c))
+            elif c == '#':
+                walls.add((x, y))
+
+
+    var blizzard_cache: seq[seq[Blizzard]]
+    blizzard_cache.add(blizzards)
+    let path = bfs(start, target, blizzard_cache, walls, width, height)
+    let start2 = newPosition(target, path.len())
+    let path2 = bfs(start2, start.pos, blizzard_cache, walls, width, height)
+    let start3 = newPosition(start.pos, path.len() + path2.len)
+    let path3 = bfs(start3, target, blizzard_cache, walls, width, height)
+    return $(path.len() + path2.len() + path3.len())
